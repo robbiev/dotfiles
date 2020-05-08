8829c763a157bc775509a904994b76ccb2a01f72
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2020-05-08 16:47:30 +0100

    disable bash usage warning

--- .bash_osx.sh
+++ .bash_osx.sh
@@ -2,3 +2,5 @@ alias plaincopy="pbpaste -Prefer txt | pbcopy"
 eval `gdircolors ~/.dir_colors`
 alias ls='gls $LS_OPTIONS -hF'
 export HOMEBREW_NO_ANALYTICS=1
+# macOS now defaults to zsh, disable warning if you're using bash
+export BASH_SILENCE_DEPRECATION_WARNING=1
