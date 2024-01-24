34d0714ae84e24e9fe25748e9206badc8d88041c
Robbie Vanbrabant <robbie@monzo.com>
2024-01-24 08:26:41 +0000

    wezterm tweaks

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,7 +9,8 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font 'Flexi IBM VGA True'
-config.font_size = 16
+config.font_size = 20
+config.bold_brightens_ansi_colors = false
 
 config.hide_tab_bar_if_only_one_tab = true
 
