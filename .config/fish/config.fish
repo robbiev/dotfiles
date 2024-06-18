2297b6be299a27c05235d93f07a253f36f96f3dc
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2024-06-18 21:18:25 +0100

    add fzf theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,30 @@ end
 set -gx LS_OPTIONS '--color=auto'
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+# folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_day.sh
+set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none
+  --color=bg+:#b6bfe2 \
+  --color=bg:#e9e9ec \
+  --color=border:#2496ac \
+  --color=fg:#3760bf \
+  --color=gutter:#e9e9ec \
+  --color=header:#b15c00 \
+  --color=hl+:#188092 \
+  --color=hl:#188092 \
+  --color=info:#8990b3 \
+  --color=marker:#d20065 \
+  --color=pointer:#d20065 \
+  --color=prompt:#188092 \
+  --color=query:#3760bf:regular \
+  --color=scrollbar:#2496ac \
+  --color=separator:#b15c00 \
+  --color=spinner:#d20065 \
+"
 set -gx PATH $HOME/bin $HOME/go/bin $PATH
 set -gx PIP_REQUIRE_VIRTUALENV true
 
