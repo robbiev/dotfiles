c80f5dd9f293b2257057c22196e8d61e6912f419
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2024-07-08 14:46:52 +0100

    latest go dev settings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -66,41 +66,40 @@ return {
       vim.keymap.set("n", "<leader>g", require("fzf-lua").live_grep_native, { noremap = true })
     end,
   },
-  -- {
-  --   "nvim-telescope/telescope.nvim",
-  --   dependencies = { "nvim-lua/plenary.nvim" },
-  --   config = function()
-  --     local builtin = require("telescope.builtin")
-  --     vim.keymap.set("n", "<leader>b", builtin.buffers, { noremap = true })
-  --     vim.keymap.set("n", "<leader>t", builtin.tags, { noremap = true })
-  --     vim.keymap.set("n", "<leader>f", builtin.find_files, { noremap = true })
-  --     vim.keymap.set("n", "<leader>g", builtin.live_grep, { noremap = true })
-  --     vim.keymap.set("n", "<leader>h", builtin.help_tags, { noremap = true })
-  --
-  --     require("telescope").setup({
-  --       defaults = {
-  --         vimgrep_arguments = {
-  --           "rg",
-  --           "--color=never",
-  --           "--no-heading",
-  --           "--with-filename",
-  --           "--line-number",
-  --           "--column",
-  --           "--smart-case",
-  --           "--hidden",
-  --           "--glob",
-  --           "!**/.git/*",
-  --         },
-  --       },
-  --       pickers = {
-  --         find_files = {
-  --           find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-  --         },
-  --       },
-  --     })
-  --
-  --   end,
-  -- },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      -- local builtin = require("telescope.builtin")
+      -- vim.keymap.set("n", "<leader>b", builtin.buffers, { noremap = true })
+      -- vim.keymap.set("n", "<leader>t", builtin.tags, { noremap = true })
+      -- vim.keymap.set("n", "<leader>f", builtin.find_files, { noremap = true })
+      -- vim.keymap.set("n", "<leader>g", builtin.live_grep, { noremap = true })
+      -- vim.keymap.set("n", "<leader>h", builtin.help_tags, { noremap = true })
+
+      require("telescope").setup({
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob",
+            "!**/.git/*",
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          },
+        },
+      })
+    end,
+  },
   {
     "airblade/vim-rooter",
     config = function()
@@ -133,9 +132,43 @@ return {
     end,
   },
   {
+    "wearedev",
+    dir = "~/src/github.com/monzo/wearedev/tools/editors/nvim/nvim-monzo",
+    cond = function(plugin)
+      local stat = vim.loop.fs_stat(plugin.dir)
+      return stat and stat.type == "directory"
+    end,
+    config = function()
+      vim.keymap.set("n", "<leader>mc", require("monzo").jump_to_component, { noremap = true })
+      vim.keymap.set("n", "<leader>mt", require("monzo").jump_to_tool, { noremap = true })
+      vim.keymap.set("n", "<leader>ml", require("monzo").jump_to_library, { noremap = true })
+      vim.keymap.set("n", "<leader>md", require("monzo").jump_to_downstream, { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>mh", "", {
+        noremap = true,
+        silent = true,
+        callback = function()
+          -- Call handlertool.
+          local file_src_path_abs = vim.fn.expand("%:p")
+          local file_src_line = vim.fn.line(".")
+          local file_src_col = vim.fn.col(".")
+          local handlertool_out = vim.fn.system(
+            "handlertool " .. vim.fn.shellescape(file_src_path_abs .. ":" .. file_src_line .. ":" .. file_src_col)
+          )
+
+          -- Parse handlertool output.
+          local file_dst_path_abs, file_dst_line, file_dst_col = string.match(handlertool_out, "([^:]+):(%d+):(%d+)")
+
+          -- Jump to the destination.
+          vim.cmd("e " .. file_dst_path_abs)
+          vim.fn.setpos(".", { vim.api.nvim_get_current_buf(), tonumber(file_dst_line), tonumber(file_dst_col), 0 })
+          vim.cmd("normal! zz")
+        end,
+      })
+    end,
+  },
+  {
     "ghostty-macos",
     dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
-    lazy = false,
     cond = function()
       return vim.fn.has("macunix") and vim.fn.executable("ghostty") == 1
     end,
